991477db91076d7306f081088fbce6e9f3f027f3
Anil Vadlapatla <anilv@live.com>
2017-09-12 01:34:46 -0700

    working n5 at approx 150th episode

--- cartpole_v0.py
+++ cartpole_v0.py
@@ -1,21 +1,28 @@
 """
 cartpole_v0 re-inforcement learning
 """
+
 import math
 from random import uniform
 import numpy as np
 import gym
 from gym import wrappers
 
-def get_observation_space_markers(observation_space, num_elements_per_dimension, scaling = 100):
+class observation_space:
+    def __init__(self, high, low):
+        self.high = high
+        self.low = low
+
+def get_observation_space_markers(observation_space, num_elements_per_dimension, scaling=1):
     """
-    given an observation_space, comes up with a dictionary for each dimension of the observation space
-        each dictionary shows the size of each segment and whether we are using a logarithmic scale
+    given an observation_space, comes up with a dictionary for each dimension of
+        the observation space
+    each dictionary shows the size of each segment and whether we are using a logarithmic scale
     """
 
     os_high = observation_space.high
     os_low = observation_space.low
-    num_dimensions = observation_space.shape[0]
+    num_dimensions = observation_space.high.shape[0]
 
     #os_segments = np.zeros(shape=(num_dimensions,num_elements_per_dimension))
     os_markers = [{} for _ in range(num_dimensions)]
@@ -55,12 +62,12 @@ def back_propagate(q_space, cells_visited, final_index, gamma, alpha_space, disp
 
     print("back propagating: ", final_index)
     index_ = final_index
-    alpha = 0.5
+    #alpha = 0.5
     for index, action, reward in reversed(cells_visited):
         if display_back_propagate:
             print("before: ", index, q_space[index], action)
 
-        #alpha = 1 / alpha_space[index]
+        alpha = 1 / alpha_space[index]
         sample = reward + gamma * max(q_space[index_])
         q_space[index][action] = (1 - alpha) * q_space[index][action] + alpha * sample
         #q_space[index][action] = sample # ignoring previous values
@@ -78,6 +85,8 @@ def get_index(observation_space_markers, observation):
     for i in range(num_dimensions):
         os_marker = observation_space_markers[i]
         obs_val = observation[i]
+        if os_marker['use_logarithmic_scale']:
+            obs_val = math.copysign(math.log10(abs(obs_val)), obs_val)
         segment_size = os_marker['segment_size']
         shift = os_marker['shift']
         index_i = math.floor( (obs_val + shift) / segment_size)
@@ -102,12 +111,13 @@ def main():
     """
     env = gym.make('CartPole-v0')
     #env = wrappers.Monitor(env, './tmp/cartpole-experiment-1', )
-    num_episodes = 100 # number of episodes we'll play
+    num_episodes = 400 # number of episodes we'll play
     max_steps_per_episode = 200
-    num_elements_per_dimension = 7 # number of discrete pieces to divide each dimension of the observation space into
-    done_reward = -200 # we want to give a large penalty if we exit the game
+    num_elements_per_dimension = 5 # number of discrete pieces to divide each dimension of the observation space into
+    done_reward = -199 # we want to give a large penalty if we exit the game
     display_back_propagate = False
 
+    # get initial OS markers
     os_markers = get_observation_space_markers(env.observation_space, num_elements_per_dimension)
     q_space_shape = [os_marker['num_segments'] for os_marker in os_markers]
     alpha_space = np.zeros(shape=tuple(q_space_shape))
@@ -118,12 +128,22 @@ def main():
 
 
     gamma = 0.6 # discount factor. higher value means we still value old data
-    backprop_decay = 0.8
+
+    #high = [float('-inf') for _ in env.observation_space.high]
+    #low = [float('inf') for _ in env.observation_space.low]
+    high = np.full_like(env.observation_space.high, float('-inf'))
+    low = np.full_like(env.observation_space.low, float('inf'))
+    os_space = observation_space(env.observation_space.high, env.observation_space.low)
 
     for i_episode in range(num_episodes):
         total_reward, reward = 0, 0
         observation = env.reset()
         index = get_index(os_markers, observation)
+        #high = [hi if hi > high[hi_index] else high[hi_index] for hi_index, hi in enumerate(observation)]
+        #low = [lo if lo < low[lo_index] else low[lo_index] for lo_index, lo in enumerate(observation)]
+        high = np.maximum(high, observation)
+        low = np.minimum(low, observation)
+
 
         cells_visited = []
 
@@ -160,6 +180,7 @@ def main():
                     .format(i_episode, t+1, alpha))
 
                 reward = total_reward + done_reward
+                #reward = done_reward
                 cells_visited[-1] = (index, action, reward)
                 if i_episode == num_episodes - 1:
                     display_back_propagate = True
@@ -171,6 +192,17 @@ def main():
 
             observation = observation_
             index = index_
+
+            high = np.maximum(high, observation)
+            low = np.minimum(low, observation)
+
+
+        #after an episode is over, re-visit creation of OS_markers based on latest data
+        if (high != os_space.high).any() or (low != os_space.low).any():
+            os_space.high = high
+            os_space.low = low
+            print("new high and low", high, low)
+            os_markers = get_observation_space_markers(os_space, num_elements_per_dimension)
 
 if __name__ == '__main__':
     main()
